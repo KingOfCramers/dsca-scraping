8e810faf44f4f9a735b047b393da19749e4c7fca
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2019-03-09 17:37:11 -0500

    finished scraper

--- app.js
+++ app.js
@@ -1,31 +1,76 @@
 const fs = require("fs");
 const pupeteer = require("puppeteer");
 const cheerio = require("cheerio");
+const path = require("path");
+const moment = require("moment");
 
-let fetchPage = async (url) => {
-    const browser = await pupeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    // await page.setViewport({ width: 1280, height: 800 });
-    await page.goto(url, { waitUntil: 'networkidle2' }); // Ensure no network requests are happening (in last 500ms).
-    let html = await page.content();
-    await browser.close();
-    return html;
+const asyncForEach = async(array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index]);
+    }
 };
 
-fetchPage('https://dod.defense.gov/News/Contracts/?Page=1')
-    .then(async(html) => {
-        let links = [];
-        $ = cheerio.load(html);
-        let dayLinks = $("div.alist div.title a");
-        dayLinks.each((i,link) => {
-            let linkPath = ($(link).attr('href'));
-            links.push(linkPath);
+const range = Array(36).fill(1).map((x, y) => x + y);
+const fetchContracts = async (url) => {
+    const browser = await pupeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' }); // Ensure no network requests are happening (in last 500ms).
+    let html = await page.content();
+
+    //// Cheerio...
+    let links = [];
+    let $ = cheerio.load(html);
+    let dayLinks = $("div.alist div.title a");
+    dayLinks.each((i,link) => {
+        let linkPath = ($(link).attr('href'));
+        links.push(linkPath);
+    });
+
+    let allFiles = [];
+
+    await asyncForEach(links, async(link) => {
+        let file = { date : '', contracts: [] };
+
+        await page.goto(link, { waitUntil: 'networkidle2' });
+        let html = await page.content();
+        let $ = cheerio.load(html);        
+        
+        let date = $('div.article-body h1');
+        let dateTextLong = ($(date).text());
+        let dateText = moment(dateTextLong.substring(14, dateTextLong.length)).format("M-D-YYYY")
+        file.date = dateText;
+
+        let contracts = $('div.article-body p');
+        contracts.each((i,contract) => {
+            let textContent = ($(contract).text());
+            file.contracts.push(textContent);
         });
-        return links;
-    })
-    .then((links) => {
-        links.forEach()
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+        allFiles.push(file)
+    });
+    await browser.close();
+    return allFiles;
+};
+
+let execute = async () => {
+    for(const pageNum in range){
+        await fetchContracts(`https://dod.defense.gov/News/Contracts/?Page=${pageNum}`)
+                .then(async(allFiles) => {
+                    asyncForEach(allFiles, async({ date, contracts }) => {
+                        var file = fs.createWriteStream(path.join(__dirname, `${date}.txt`));        
+                        file.on('error', (err) => { 
+                            throw err;
+                        });
+                        contracts.forEach((p) => { 
+                            file.write(p.concat(' \n')); 
+                        });
+
+                        file.end();
+                    })
+                })
+                .catch((err) => console.log(err));
+        console.log(`Fetched and wrote contracts for ${pageNum}...`);
+    };
+    console.log('Done.')
+}
+
+execute();
